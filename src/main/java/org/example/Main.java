06b2136af9faf73d44c5aef5514701ea42a9fcdb
unknown <malakalkindi1995@gmail.com>
2023-04-09 14:16:54 +0400

    fetch data from api insert to DB but the date

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,33 @@ import java.net.ProtocolException;
 import java.net.URL;
 import com.google.gson.Gson;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=5TMCX2qSvFGtY00sNkxjAsBSjfUUvUBS";
-        System.out.println("Hello world!");
-        newsMain newsMain = new newsMain();
+        String urll = "jdbc:sqlserver://localhost:1433;" +
+                "databaseName=ff;" +
+                "encrypt=true;" +
+                "trustServerCertificate=true";
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the user to access the database");
+        String user = scanner.nextLine()  ;
+        System.out.println("enter the user the password");
+        String pass = scanner.nextLine()  ;
+        newsMain newsMain = new newsMain();
+        Connection con = null;
         try {
+            Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+            DriverManager.registerDriver(driver);
+            con = DriverManager.getConnection(urll, user, pass);
+            Statement st = con.createStatement();
+
             URL url = new URL(apiUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -42,9 +61,13 @@ public class Main {
             response r =m.response;
             System.out.println("\n\nproperties of class docs   ");
             int di =1;
+            Statement stt = con.createStatement();
             for (docs d : r.getDocs()) {
-
+                String sql = "insert into NewsAggregator values('" + d.getHeadline().getMain()
+                        + "','" + d.getSource() + "','" + "1994/02/02" + "','"+d.getDocument_type()+"','"+d.getLead_paragraph()+"')";
+                stt.execute(sql);
                 System.out.println("\n\nproperties of class docs " +di);
+                stt.execute(sql);
                 int mmi =1;
                 for (multimedia  mm : d.getMultimedia()) {
 
@@ -84,12 +107,24 @@ di++;
 
             //name[] rr = gson.toJson(nam.name.get(0), name[].class);
 
+
+//            String sql = "insert into hotels values (?, ?, ?, ?, ?, ?)";
+//            PreparedStatement pstmt = con.prepareStatement(sql);
+            con.close();
         } catch (ProtocolException ex) {
             throw new RuntimeException(ex);
         } catch (MalformedURLException ex) {
             throw new RuntimeException(ex);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
 
         //	name[] Nam = gson.fromJson(json.toString(),  parent[0].name[].class);
